46bfdbb08c02cc2aa67837b6e51f33d9b243b1eb
nkubana0 <i.shema@alustudent.com>
2024-03-23 12:44:16 +0200

    unlimited messages

---
+++ Unlimited-texts/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+while True:
+    time.sleep(4)
+    pyautogui.typewrite("Yoo nigga")
+    time.sleep(2)
+    pyautogui.press('enter')
---
+++ Unlimited-texts/texts.py
@@ -0,0 +1,10 @@
+#Sends unlimited messages on whatsapp and messanger
+#from tech2etc
+
+import pyautogui
+import time
+while True:
+    time.sleep(4)
+    pyautogui.typewrite("Hey! Watsup")
+    time.sleep(2)
+    pyautogui.press('enter')
